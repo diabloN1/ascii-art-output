7402ce5efb595880f0ae6b10e2802244f595e806
amineYc <amineamoune904@gmail.com>
2024-09-17 17:40:02 +0100

    reWrite byteToaAsciiMap function with strings.Split

--- myFunctions/bytesToAsciiMap.go
+++ myFunctions/bytesToAsciiMap.go
@@ -1,28 +1,15 @@
 package myfunctions
 
+import "strings"
+
 func BytesToAsciiMap(style []byte) map[int]string {
 	chars := make(map[int]string)
-	line := 1
-	next := 9
-	char := ""
 	intValue := 32
-
-	//Range byte by byte standard to fill each ascii separatly in the map.
-	for i := 1; i < len(style) - 1; i++ {
-		if i < len(style) - 2 {
-		 	if style[i] == '\n' {
-				line++
-			} else if line == next+1 {
-				next += 9
-				chars[intValue] = char[:len(char)-2]
-				intValue++
-				char = ""
-			}
-			char += string(style[i])
-		} else {
-			char += string(style[i])
-			chars[intValue] = char
+	splitedStyle := strings.Split(string(style[1:]), "\n\n")
+	//Range char by char standard to fill each ascii separatly in the map.
+		for _, v := range splitedStyle {
+			chars[intValue] = v
+			intValue++
 		}
-	}
 	return chars
 }
